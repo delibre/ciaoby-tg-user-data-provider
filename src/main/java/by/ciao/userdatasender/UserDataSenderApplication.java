79636b62a539fd2a3295761fa93fa650a6fcb9ef
delibre <viacheslav.bylo@gmail.com>
2023-04-19 18:49:09 +0200

    project created

---
+++ src/main/java/by/ciao/userdatasender/UserDataSenderApplication.java
@@ -0,0 +1,13 @@
+package by.ciao.userdatasender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDataSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/ciao/userdatasender/UserDataSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ciao.userdatasender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
